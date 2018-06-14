37031341812f522a0b12768a463f69d0abe38b1a
mikhawa <michaeljpitz@gmail.com>
2018-06-14 11:53:55 +0200

    Public and Admin controller

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
  * Front controller
  */
 
+// session
+session_start();
+
 // dependance
 require_once "config.php";
 
@@ -10,3 +13,16 @@ require_once "config.php";
 spl_autoload_register(function($nameClass){
     require_once "Model/$nameClass.class.php";
 });
+
+// pre routing
+// if connected
+if(isset($_SESSION['monid'])&&$_SESSION['monid']==session_id()){
+
+    require_once "Controller/AdminController.php";
+
+// if public
+}else{
+
+    require_once "Controller/PublicController.php";
+
+}
